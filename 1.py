0be30506e9002b6ac8efc8bd94f772ce03eaf86d
Venturh <venturh94@gmail.com>
2019-04-24 10:39:48 +0200

    test

--- 1.py
+++ 1.py
@@ -5,10 +5,17 @@ HINTERGRUNDFARBE = 0
 FIELDOFVIEW = 45
 ASPECTRATIO = 10
 
-# Auge
+# Kamera
 e = np.array([0, 1.8, 10])
 c = np.array([0, 3, 0])
 up = np.array([0, 1, 0])
+
+# Koordinatensystem
+cminusE = c - e
+f = cminusE / math.sqrt(cminusE[0] ** 2 + cminusE[1] ** 2 + cminusE[2] ** 2)
+fKreuzUp = np.cross(f, up)
+s = fKreuzUp / math.sqrt(fKreuzUp[0] ** 2 + fKreuzUp[1] ** 2 + fKreuzUp[2] ** 2)
+u = np.cross(s,f)
 
 
 class Ray(object):
@@ -26,18 +33,16 @@ def calcray(x, y):
 
 # Betrachtergeometrie
 alpha = FIELDOFVIEW / 2
+w = 400
+w = 400
+
 height = 2 * math.tan(alpha)
 width = ASPECTRATIO * height
 
 wRes = 400
 hRes = 400
 
-# Koordinatensystem
-cminusE = c - e
-f = cminusE / math.sqrt(cminusE[0] ** 2 + cminusE[1] ** 2 + cminusE[2] ** 2)
-fKreuzUp = np.cross(f, up)
-s = fKreuzUp / math.sqrt(fKreuzUp[0] ** 2 + fKreuzUp[1] ** 2 + fKreuzUp[2] ** 2)
-u = np.cross(s,f)
+
 
 #Betrachtergeometrie
 pixelWidth = width / (wRes - 1)
