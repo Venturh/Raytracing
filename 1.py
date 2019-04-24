c21d6c6077baf8c101b4a6b79e68bc7a07dced0c
Anonymer Eintrag <mwerp001@pc13-115.intern.mi.hs-rm.de>
2019-04-24 10:10:49 +0200

    Initial commit

---
+++ 1.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+
+HINTERGRUNDFARBE = 0
+FIELDOFVIEW = 45
+ASPECTRATIO = 10
+
+# Auge
+e = np.array([0, 1.8, 10])
+c = np.array([0, 3, 0])
+up = np.array([0, 1, 0])
+
+
+class Ray(object):
+    def __init__(self, ursprung, richtung):
+        self.ursprung = ursprung
+        self.richtung = richtung.normalized()  # laenge 1
+
+    def punktbeiparameter(self, t):
+        return self.ursprung + self.richtung.scale(t)
+
+
+def calcray(x, y):
+    return 0
+
+
+# Betrachtergeometrie
+alpha = FIELDOFVIEW / 2
+height = 2 * math.tan(alpha)
+width = ASPECTRATIO * height
+
+wRes = 400
+hRes = 400
+
+# Koordinatensystem
+cminusE = c - e
+f = cminusE / math.sqrt(cminusE[0] ** 2 + cminusE[1] ** 2 + cminusE[2] ** 2)
+fKreuzUp = np.cross(f, up)
+s = fKreuzUp / math.sqrt(fKreuzUp[0] ** 2 + fKreuzUp[1] ** 2 + fKreuzUp[2] ** 2)
+u = np.cross(s,f)
+
+#Betrachtergeometrie
+pixelWidth = width / (wRes - 1)
+pixelHeight = height / (hRes - 1)
+for y in range(hRes):
+    for x in range(wRes):
+        xcomp = s.scale(x * pixelWidth - width / 2)
+        ycomp = s.scale(y * pixelHeight - height / 2)
+        ray = Ray(e, f + xcomp + ycomp)
+
+# RayCasting
+for x in range(400):
+    for y in range(400):
+        ray = calcray(x, y)
+        maxdist = float('inf')
+        farbe = HINTERGRUNDFARBE
+        for object in objectlist:
+            hitdist = object.intersectionParameter(ray)
+            if hitdist:
+                if hitdist < maxdist:
+                    maxdist = hitdist
+                    farbe = object.colorAt(ray)
+        image.putpixel((x, y), farbe)
